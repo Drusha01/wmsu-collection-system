00f3b7762b8a71dc703aaf12e489b9ddd5154340
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2024-03-24 02:36:00 +0800

    npm install. bug fix

--- app/Livewire/Admin/Students/Students.php
+++ app/Livewire/Admin/Students/Students.php
@@ -54,55 +54,30 @@ class Students extends Component
             $this->resetPage();
             $this->prevstudent_id_search = $this->student_id_search;
         }
-        if(strlen($this->student_id_search)>0){
-            $student_data = DB::table('students as s')
-                ->select(
-                    "s.id",
-                    "student_code",
-                    "first_name",
-                    "middle_name",
-                    "last_name",
-                    "email",
-                    "s.college_id",
-                    "s.department_id",
-                    "s.date_created",
-                    "s.date_updated",
-                    "c.code as college_code",
-                    "c.name as college_name",
-                    "d.name as department_name",
-                    "d.code as department_code",
-                    's.is_muslim',
-                    's.is_active'
-                )
-                ->join('colleges as c','s.college_id','c.id')
-                ->join('departments as d','s.department_id','d.id')
-                ->where('s.student_code','like',$this->student_id_search.'%')
-                ->paginate(10);
-        }
-        else{
-            $student_data = DB::table('students as s')
-                ->select(
-                    "s.id",
-                    "student_code",
-                    "first_name",
-                    "middle_name",
-                    "last_name",
-                    "email",
-                    "s.college_id",
-                    "s.department_id",
-                    "s.date_created",
-                    "s.date_updated",
-                    "c.code as college_code",
-                    "c.name as college_name",
-                    "d.name as department_name",
-                    "d.code as department_code",
-                    's.is_muslim',
-                    's.is_active'
-                )
-                ->join('colleges as c','s.college_id','c.id')
-                ->join('departments as d','s.department_id','d.id')
-                ->paginate(10);
-        }
+        $student_data = DB::table('students as s')
+            ->select(
+                "s.id",
+                "student_code",
+                "first_name",
+                "middle_name",
+                "last_name",
+                "email",
+                "s.college_id",
+                "s.department_id",
+                "s.date_created",
+                "s.date_updated",
+                "c.code as college_code",
+                "c.name as college_name",
+                "d.name as department_name",
+                "d.code as department_code",
+                's.is_muslim',
+                's.is_active'
+            )
+            ->join('colleges as c','s.college_id','c.id')
+            ->join('departments as d','s.department_id','d.id')
+            ->where('s.student_code','like',$this->student_id_search.'%')
+            ->paginate(10);
+        
         
         $colleges_data = DB::table('colleges')
             ->get()
