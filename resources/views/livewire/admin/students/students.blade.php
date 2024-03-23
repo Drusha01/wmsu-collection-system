2c4f66cae348cd6fa29acf9fae47281f7ca74583
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2024-03-24 05:51:29 +0800

    removed filter

--- resources/views/livewire/admin/students/students.blade.php
+++ resources/views/livewire/admin/students/students.blade.php
@@ -53,56 +53,7 @@
                             </form>
                         </div>
                         <div class="flex items-center space-x-3 w-full md:w-auto">
-                            <button id="filterDropdownButton" data-dropdown-toggle="filterDropdown"
-                                class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                                type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
-                                    class="h-4 w-4 mr-2 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                        d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                                Filter
-                                <svg class="-mr-1 ml-1.5 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                    <path clip-rule="evenodd" fill-rule="evenodd"
-                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
-                                </svg>
-                            </button>
-                            <div id="filterDropdown"
-                                class="z-10 hidden w-48 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
-                                <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">School Year</h6>
-                                <ul class="space-y-2 text-sm" aria-labelledby="filterDropdownButton">
-                                    <li class="flex items-center">
-                                        <input id="firstyear" type="checkbox" value=""
-                                            class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                        <label for="firstyear"
-                                            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">First
-                                            Year</label>
-                                    </li>
-                                    <li class="flex items-center">
-                                        <input id="secondyear" type="checkbox" value=""
-                                            class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                        <label for="secondyear"
-                                            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">Second
-                                            Year</label>
-                                    </li>
-                                    <li class="flex items-center">
-                                        <input id="thirdyear" type="checkbox" value=""
-                                            class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                        <label for="thirdyear"
-                                            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">Third
-                                            Year</label>
-                                    </li>
-                                    <li class="flex items-center">
-                                        <input id="fourthyear" type="checkbox" value=""
-                                            class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                        <label for="fourthyear"
-                                            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">Fourth
-                                            Year</label>
-                                    </li>
-                                </ul>
-                            </div>
+                           
                             <button style="display:none" id="activateStudentModalToggler" data-modal-target="activateStudentModal" data-modal-toggle="activateStudentModal">asdf</button>
                             <button style="display:none" id="deleteStudentModalToggler" data-modal-target="deleteStudentModal" data-modal-toggle="deleteStudentModal">asdf</button>
                             <button style="display:none" id="viewStudentModalToggler" data-modal-target="viewStudentModal" data-modal-toggle="viewStudentModal">asdf</button>
