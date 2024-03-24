bc3ec5c119d721e442cec8a254268ee663dad817
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2024-03-25 05:18:00 +0800

    usc with debounce

--- resources/views/livewire/usc/fees/fees.blade.php
+++ resources/views/livewire/usc/fees/fees.blade.php
@@ -54,7 +54,7 @@
                                         </svg>
                                     </div>
                                     <form wire:click.prevent="search()">
-                                        <input type="text" id="simple-search" wire:model.live="filters.fee_name"
+                                        <input type="text" id="simple-search" wire:model.live.debounce.250ms="filters.fee_name"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                         placeholder="Search fee name" required="">
                                     </form>
