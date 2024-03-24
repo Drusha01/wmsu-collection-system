6304a699ea55a3537cabda4ff35b4e0ef604fcb8
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2024-03-25 05:01:43 +0800

    usc filters

--- resources/views/livewire/usc/fees/fees.blade.php
+++ resources/views/livewire/usc/fees/fees.blade.php
@@ -53,9 +53,11 @@
                                                 clip-rule="evenodd" />
                                         </svg>
                                     </div>
-                                    <input type="text" id="simple-search"
+                                    <form wire:click.prevent="search()">
+                                        <input type="text" id="simple-search" wire:model.live="filters.fee_name"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        placeholder="Search" required="">
+                                        placeholder="Search fee name" required="">
+                                    </form>
                                 </div>
                             </form>
                         </div>
@@ -318,9 +320,9 @@
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th scope="col" class="px-4 py-3">ID</th>
+                                    <th scope="col" class="px-4 py-3">Fee Name</th>
                                     <th scope="col" class="px-4 py-3">Fee Type</th>
                                     <th scope="col" class="px-4 py-3">Fee Code</th>
-                                    <th scope="col" class="px-4 py-3">Fee Name</th>
                                     <th scope="col" class="px-4 py-3">For Muslim?</th>
                                     <th scope="col" class="px-4 py-3">Academic Year</th>
                                     <th scope="col" class="px-4 py-3">Semester</th>
@@ -334,10 +336,10 @@
                             <tbody>
                                 @foreach($university_fees_data as $key =>$value)
                                     <tr class="border-b dark:border-gray-700">
-                                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{($university_fees_data->currentPage()-1)*$university_fees_data->perPage()+$key+1 }}</th>
+                                        <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{($university_fees_data->currentPage()-1)*$university_fees_data->perPage()+$key+1 }}</th>
+                                        <td class="px-4 py-3">{{$value->name}}</td>
                                         <td class="px-4 py-3">University Fee</td>
                                         <td class="px-4 py-3">{{$value->code}}</td>
-                                        <td class="px-4 py-3">{{$value->name}}</td>
                                         <td class="px-4 py-3">@if($value->for_muslim) Yes @else No @endif</td>
                                         <td class="px-4 py-3">{{$value->year_start.' - '.$value->year_end}}</td>
                                         <td class="px-4 py-3">{{$value->semester}}</td>
