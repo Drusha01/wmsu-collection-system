3b5ff50965961248f7a69f699214512bf6f2ad3a
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2024-03-26 07:30:40 +0800

    csc dashboard

--- app/Livewire/Csc/Dashboard/Dashboard.php
+++ app/Livewire/Csc/Dashboard/Dashboard.php
@@ -16,6 +16,7 @@ class Dashboard extends Component
         'semester_id'=>NULL,
     ];
     public $dashboard_data = [
+        'total_remitted' =>0,
         'total_collected' =>0,
         'usc_shares' => 0,
         'csc_shares' => 0,
@@ -134,6 +135,14 @@ class Dashboard extends Component
             ->where('es.college_id','=',$this->user_details->college_id)
             ->where('es.semester_id','=',$this->filters['semester_id'])
             ->first();
+        $total_remitted = DB::table('remits as r')
+            ->select(
+                DB::raw('sum(amount) as total_remitted')
+            )
+            ->where('r.school_year_id','=',$this->user_details->school_year_id)
+            ->where('r.college_id','=',$this->user_details->college_id)
+            ->where('appoved_by','>',0)
+            ->first();
         if($total_collected){
             $this->dashboard_data['total_collected'] = $total_collected->total_collected;
         }
@@ -147,40 +156,44 @@ class Dashboard extends Component
             $this->dashboard_data['number_of_enrolled_students'] = $number_of_enrolled_students->number_of_enrolled_students;
         }
 
+        if($total_remitted){
+            $this->dashboard_data['total_remitted'] = $total_remitted->total_remitted;
+        }
         $payment_records_data = DB::table('payment_items as pi')
-        ->select(
-            "pi.id as id",
-            "u.id as user_id",
-            "u.first_name as collector_first_name",
-            "u.middle_name as collector_middle_name",
-            "u.last_name as collector_last_name",
-            "u.username as collector_username",
-            "s.id as student_id",
-            "s.student_code as student_code",
-            "s.first_name as student_first_name",
-            "s.middle_name as student_middle_name",
-            "s.last_name as student_last_name",
-            "f.name as fee_name",
-            "f.code as fee_code",
-            "ft.name as fee_type_name",
-            "pi.amount",
-            'pi.date_created as date_created'
-        )
-        ->join('students as s','s.id','pi.student_id')
-        ->join('enrolled_students as es','s.id','es.student_id')
-        ->join('users as u','u.id','pi.collected_by')
-        ->join('fees as f','f.id','pi.fee_id')
-        ->rightjoin('fee_types as ft','ft.id','f.fee_type_id')
-        ->where('es.college_id','=',$this->user_details->college_id)
-        ->where('f.school_year_id','=',$this->user_details->school_year_id)
-        ->where('f.semester_id','=',$this->filters['semester_id'] )
-        ->orderBy('pi.date_created','desc')
-        ->groupBy('pi.id')
-        ->limit(5)
-        ->get()
-        ->toArray();
-    
-        // dd($this->dashboard_data);
+            ->select(
+                "pi.id as id",
+                "u.id as user_id",
+                "u.first_name as collector_first_name",
+                "u.middle_name as collector_middle_name",
+                "u.last_name as collector_last_name",
+                "u.username as collector_username",
+                "s.id as student_id",
+                "s.student_code as student_code",
+                "s.first_name as student_first_name",
+                "s.middle_name as student_middle_name",
+                "s.last_name as student_last_name",
+                "f.name as fee_name",
+                "f.code as fee_code",
+                "ft.name as fee_type_name",
+                "pi.amount",
+                'pi.date_created as date_created'
+            )
+            ->join('students as s','s.id','pi.student_id')
+            ->join('enrolled_students as es','s.id','es.student_id')
+            ->join('users as u','u.id','pi.collected_by')
+            ->join('fees as f','f.id','pi.fee_id')
+            ->rightjoin('fee_types as ft','ft.id','f.fee_type_id')
+            ->where('es.college_id','=',$this->user_details->college_id)
+            ->where('f.school_year_id','=',$this->user_details->school_year_id)
+            ->where('f.semester_id','=',$this->filters['semester_id'] )
+            ->orderBy('pi.date_created','desc')
+            ->groupBy('pi.id')
+            ->limit(5)
+            ->get()
+            ->toArray();
+
+       
+        
         $page_info = DB::table('users as u')
         ->select(
             'c.name as college_name',
