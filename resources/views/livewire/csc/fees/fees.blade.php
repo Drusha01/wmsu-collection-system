f293dfbb0aa723bff11e4682df4eab2512c3a961
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2024-03-25 09:13:30 +0800

    fees

--- resources/views/livewire/csc/fees/fees.blade.php
+++ resources/views/livewire/csc/fees/fees.blade.php
@@ -41,8 +41,7 @@
                     </div>
                     <!--End Breadcrumb -->
                     <!--Table Header -->
-                    <div
-                        class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4 mt-8">
+                    <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4 mt-8">
                         <div class="w-full md:w-1/4">
                             <form class="flex items-center">
                                 <label for="simple-search" class="sr-only">Search</label>
