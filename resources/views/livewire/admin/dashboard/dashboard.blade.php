943918f8b8f60510d76c23879e389086952085b7
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2024-03-27 23:52:32 +0800

    dashboard-spaces-changes

--- resources/views/livewire/admin/dashboard/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard/dashboard.blade.php
@@ -47,7 +47,7 @@
                             </div>
                         </div>
                     </div> 
-                    <div class="col-span-1 items-center justify-between p-4 bg-white border ml-4 border-gray-200 rounded-lg shadow-sm sm:flex dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+                    <div class="mr-4 col-span-1 items-center justify-between p-4 bg-white border ml-4 border-gray-200 rounded-lg shadow-sm sm:flex dark:border-gray-700 sm:p-6 dark:bg-gray-800">
                         <div class="w-full">
                             <h3 class="text-base font-normal text-gray-500 dark:text-gray-400">Total Collected </h3>
                             <span class="text-2xl font-bold leading-none text-gray-900 sm:text-3xl dark:text-white">PHP {{number_format($dashboard_data['total_collected'], 2, '.', ',')}} </span>
