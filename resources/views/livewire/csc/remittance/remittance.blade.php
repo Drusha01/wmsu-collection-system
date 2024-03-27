ce28f1019dc9d37b01e2eae9039e3662ba29da31
Hanrickson E. Dumapit <114381735+Drusha01@users.noreply.github.com>
2024-03-27 10:09:55 +0800

    debounce, remove the auto clearing letters

--- resources/views/livewire/csc/remittance/remittance.blade.php
+++ resources/views/livewire/csc/remittance/remittance.blade.php
@@ -39,7 +39,6 @@
                     <!--Table Header -->
                     <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                         <div class="w-full md:w-1/4">
-                            <form class="flex items-center">
                                 <label for="simple-search" class="sr-only">Search</label>
                                 <div class="relative w-full">
                                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -49,7 +48,6 @@
                                     </div>
                                     <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
                                 </div>
-                            </form>
                         </div>
                         <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
                             <div class="flex items-center space-x-3 w-full md:w-auto">
