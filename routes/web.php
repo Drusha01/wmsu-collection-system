57e2b7fb7cf055c40001623ab0f3bcd43cc368cc
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2024-03-29 00:57:40 +0800

    profile

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ use App\Livewire\Admin\RemitRecords\RemitRecords as AdminRemitRecords;
 use App\Livewire\Admin\Settings\Fees\Fees as AdminFees;
 use App\Livewire\Admin\Settings\Overview\Overview as AdminOverview;
 use App\Livewire\Admin\Settings\UserManagement\UserManagement as AdminUserManagement;
+use App\Livewire\Admin\Settings\Profile\Profile as AdminProfile;
 use App\Livewire\Admin\Students\Students as AdminStudents;
 use App\Livewire\Admin\SystemLogs\SystemLogs as AdminSystemLogs;
 
@@ -42,7 +43,7 @@ use App\Livewire\Usc\Fees\Fees as UscFees;
 use App\Livewire\Usc\Paymentrecords\Paymentrecords as UscPaymentrecords;
 use App\Livewire\Usc\Remitrecords\Remitrecords as UscRemitrecords;
 use App\Livewire\Usc\Remittance\Remittance as UscRemittance;
-
+use App\Livewire\Usc\Profile\Profile as UscProfile;
 
 use App\Livewire\Csc\Auditlogs\Auditlogs as CscAuditlogs;
 use App\Livewire\Csc\Dashboard\Dashboard as CscDashboard;
@@ -54,12 +55,15 @@ use App\Livewire\Csc\Payments\Payments as CscPayments;
 use App\Livewire\Csc\Remitrecords\Remitrecords as CscRemitrecords;
 use App\Livewire\Csc\Remittance\Remittance as CscRemittance;
 use App\Livewire\Csc\Payments\StudentPayments as CscStudentPayments;
-
+use App\Livewire\Csc\Profile\Profile as CscProfile;
 
 use App\Livewire\Csc\Dashboard\Dashboard;
 use App\Livewire\Csc\Payments\Payments;
 use App\Livewire\Csc\Paymentrecords\Paymentrecords;
 use App\Livewire\Csc\Auditlogs\Auditlogs;
+use App\Livewire\Csc\Profile\Profile;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -101,6 +105,8 @@ Route::middleware([AccountisValid::class,isAdmin::class])->group(function () {
         Route::get('/fees', AdminFees::class)->name('admin-fees');
         Route::get('/overview', AdminOverview::class)->name('admin-overview');
         Route::get('/usermanagement', AdminUserManagement::class)->name('admin-usermanagement');
+        Route::get('/profile', AdminProfile::class)->name('admin-profile');
+
     });
 });
 
@@ -112,6 +118,8 @@ Route::middleware([AccountisValid::class,isUsc::class,checkTerm::class])->group(
         Route::get('/remitrecords', UscRemitRecords::class)->name('usc-remitrecords');
        
         Route::get('/fees', UscFees::class)->name('usc-fees');
+        Route::get('/profile', UscProfile::class)->name('usc-profile');
+
     });
 });
 
@@ -129,6 +137,7 @@ Route::middleware([AccountisValid::class,isCsc::class,isCollector::class,checkTe
         Route::get('/auditlogs', CscAuditLogs::class)->name('csc-auditlogs');
       
         Route::get('/fees', CscFees::class)->name('csc-fees');
+        Route::get('/profile', CscProfile::class)->name('csc-profile');
 
     });
 });
@@ -141,5 +150,7 @@ Route::middleware([AccountisValid::class,isCsc::class,checkTerm::class])->group(
         Route::get('/payments/{student_id}/{semester}', CscStudentPayments::class)->name('csc-collector-student-payments-semester');
         Route::get('/paymentrecords', PaymentRecords::class)->name('csc-collector-paymentrecords');
         Route::get('/auditlogs', AuditLogs::class)->name('csc-collector-auditlogs');
+        Route::get('/profile]', Profile::class)->name('csc-collector-profile');
+
     });
 });
