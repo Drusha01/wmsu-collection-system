4d61c911bbefc856100e4e282e0108b437fa4761
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2024-03-31 00:52:51 +0800

    route-for-breadcrumb-except-for-collector-side-may-triny-ako-nakacomment

--- resources/views/livewire/admin/settings/fees/fees.blade.php
+++ resources/views/livewire/admin/settings/fees/fees.blade.php
@@ -9,7 +9,7 @@
                     <nav class="flex px-5 py-3 text-gray-700" aria-label="Breadcrumb">
                         <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
                             <li class="inline-flex items-center">
-                                <span class="inline-flex items-center text-sm font-medium text-gray-700 dark:text-gray-400">
+                                <span class="inline-flex items-center text-sm font-medium text-gray-500 dark:text-gray-400">
                                     <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                         fill="currentColor" viewBox="0 0 20 20">
                                         <path
@@ -19,16 +19,16 @@
                                 </span>
                             </li>
                             <li aria-current="page">
-                                <div class="flex items-center">
+                                <a href="{{route('admin-fees')}}" class="flex items-center">
                                     <svg class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                             stroke-width="2" d="m1 9 4-4-4-4" />
                                     </svg>
                                     <span
-                                        class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                                        class="ms-1 text-sm font-medium text-gray-700 md:ms-2 dark:text-gray-400">
                                         Fees</span>
-                                </div>
+                                </a>
                             </li>
                         </ol>
                     </nav>
